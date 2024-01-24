7949c0cc42741d94d0ebd1ff1eb0f9e91ab97de3
Riccardo Porreca <riccardo.porreca@mirai-solutions.com>
2024-01-24 09:25:40 +0100

    Major refresh of CI-CD workflow and deployment setup
    * Based on miraisolutions/ShinyCICD#19

---
+++ deploy/deploy-shinyapps.R
@@ -0,0 +1,14 @@
+# deploy/deploy-shinyapps.R
+# usethis::use_build_ignore("deploy")
+if (!interactive()) {
+  rsconnect::setAccountInfo(
+    Sys.getenv("SHINYAPPS_ACCOUNT"),
+    Sys.getenv("SHINYAPPS_TOKEN"),
+    Sys.getenv("SHINYAPPS_SECRET")
+  )
+}
+# Add here any additional files/directories the app needs
+app_files = c("app.R", "DESCRIPTION", "NAMESPACE", "R/", "inst/")
+rsconnect::deployApp(
+  appName = "rTRhexNG", appFiles = app_files, forceUpdate = TRUE
+)
